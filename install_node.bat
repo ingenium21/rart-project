b8f0a204f924ed035e2da4573432ce371f8120f5
zlecawiigo <zlecawiigo@users.noreply.github.com>
2020-06-18 14:10:10 +0530

    remove REM

--- install_node.bat
+++ install_node.bat
@@ -1,7 +1,9 @@
-REM curl https://nodejs.org/dist/v14.4.0/node-v14.4.0-win-x64.zip -o node.zip
-REM tar -xvf node.zip
+curl https://nodejs.org/dist/v14.4.0/node-v14.4.0-win-x64.zip -o node.zip
+tar -xvf node.zip
 SET CURR_DIR=%CD%
-REM MV node-v14.4.0-win-x64 node
+MV node-v14.4.0-win-x64 node
 CD node\
 CALL .\nodevars.bat
 CD /d "%CURR_DIR%"
+CD frontend\
+npm install
