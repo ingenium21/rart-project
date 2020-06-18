d3ccdce2e3598bb9df5c6c5bc17041185097c80d
zlecawiigo <zlecawiigo@users.noreply.github.com>
2020-06-18 20:37:20 +0530

    Fix cp/COPY issue

--- App/run.bat
+++ App/run.bat
@@ -1,9 +1,10 @@
 CD ..\frontend
 CALL .\build.bat
+COPY build\index.html ..\App\data\
 CD ..\App
 SET GOPATH=%CD%\..\gopath
 SET GOROOT=%CD%\..\go
-SET PATH=%PATH%;%GOROOT%\bin
+SET PATH=%GOROOT%\bin;%PATH%;
 %GOPATH%\bin\pkger.exe
 ..\go\bin\go.exe build -o app.exe main.go helpers.go pkged.go
 app.exe
--- install_node.bat
+++ install_node.bat
@@ -1,7 +1,7 @@
 curl https://nodejs.org/dist/v14.4.0/node-v14.4.0-win-x64.zip -o node.zip
 tar -xvf node.zip
-SET CURR_DIR=%CD%
 MV node-v14.4.0-win-x64 node
+SET CURR_DIR=%CD%
 CD node\
 CALL .\nodevars.bat
 CD /d "%CURR_DIR%"
