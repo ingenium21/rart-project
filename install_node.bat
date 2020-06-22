a3913ba51d3c501c9c97f8581dd58e83e1c5dd2d
Renato Regalado <renato.regalado@gmail.com>
2020-06-21 19:22:31 -0500

    Fixed move command in install_node.bat

--- install_node.bat
+++ install_node.bat
@@ -1,6 +1,6 @@
 curl https://nodejs.org/dist/v14.4.0/node-v14.4.0-win-x64.zip -o node.zip
 tar -xvf node.zip
-MV node-v14.4.0-win-x64 node
+MOVE node-v14.4.0-win-x64 node
 SET CURR_DIR=%CD%
 CD node\
 CALL .\nodevars.bat
